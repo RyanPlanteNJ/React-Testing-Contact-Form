0fbd7848f0dc9aac100cc2f1b1a1e632915c389c
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-05-14 22:28:13 -0400

    not messing up this time

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByText, getByTestId, waitFor } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("fill form", async () => {
+
+  const fillform = {firstName: "Ryan", lastName: "Plante", email: "ryanm.plante@gmail.com", message: "MegLee"};
+  const { getByText, getBytestID } = render(<App/>) 
+
+  const firstName = getByText("First Name*");
+  firstName.value = fillform.firstName;
+    expect(firstName.value).toBe("Ryan");
+
+  const lastName = getByText("Last Name*");
+  lastName.value = fillform.lastName;
+    expect(lastName.value).toBe("Plante");
+
+  const email = getByText("Email*");
+  email.value = fillform.email;
+    expect(email.value).toBe("ryanm.plante@gmail.com");
+  
+    const message = getByText("Message");
+    message.value = fillform.message;
+      expect(message.value).toBe(fillform.message);
+
+})
